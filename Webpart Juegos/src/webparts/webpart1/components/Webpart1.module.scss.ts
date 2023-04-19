599af1f02879f59cbc6788c86cede61b07df12e0
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2023-04-19 17:55:00 +0200

    Cambios 19/04
    Añadido una entrada de datos en mi sitio para añadir en listas los datos que metan los usuarios

--- Webpart Juegos/src/webparts/webpart1/components/Webpart1.module.scss.ts
+++ Webpart Juegos/src/webparts/webpart1/components/Webpart1.module.scss.ts
@@ -1,11 +1,13 @@
 /* tslint:disable */
 require("./Webpart1.module.css");
 const styles = {
-  webpart1: 'webpart1_1f7a7f92',
-  teams: 'teams_1f7a7f92',
-  welcome: 'welcome_1f7a7f92',
-  welcomeImage: 'welcomeImage_1f7a7f92',
-  links: 'links_1f7a7f92'
+  webpart1: 'webpart1_32acceff',
+  teams: 'teams_32acceff',
+  welcome: 'welcome_32acceff',
+  Table: 'Table_32acceff',
+  boton1: 'boton1_32acceff',
+  welcomeImage: 'welcomeImage_32acceff',
+  links: 'links_32acceff'
 };
 
 export default styles;
--- Webpart Juegos/src/webparts/webpart1/components/Webpart1.tsx
+++ Webpart Juegos/src/webparts/webpart1/components/Webpart1.tsx
@@ -7,50 +7,59 @@ import styles from "./Webpart1.module.scss"
 import { TextField } from '@fluentui/react/lib/TextField';
 import { IStackProps, IStackStyles, IStackTokens, Stack } from '@fluentui/react/lib/Stack';
 import { Dropdown, DropdownMenuItemType, IDropdownStyles, IDropdownOption } from '@fluentui/react/lib/Dropdown';
-import { DatePicker, DayOfWeek, defaultDatePickerStrings, } from '@fluentui/react';
 import * as moment from "moment"
-  //Definición de las columnas y petición para extraer los datos de la misma.
-  const columns = [
-    {
-      title: 'Nombre',
-      dataIndex: 'Title',
-      key: 'Title',
-    },
-    {
-      title: 'Descripción',
-      dataIndex: 'J_Desc',
-      key: 'J_Desc',
-    },
-    {
-      title: 'Fecha lanzamiento',
-      dataIndex: 'J_Lanz',
-      key: 'J_Lanz',
-      render: (dateString: string) => moment(dateString).format('DD/MM/YYYY'),
-    },
-    {
-      title: 'Género',
-      dataIndex: 'J_Gen',
-      key: 'J_Gen',
-    },
-    {
-      title: 'Duración',
-      dataIndex: 'J_Dur',
-      key: 'J_Dur',
-    },
-  ];
-  interface JuegoItem {
-    Title: string;
-    J_Desc: string;
-    J_Dur: string;
-    J_Gen: string;
-    J_Lanz: string;
-  }
-  export interface DataType {
-    key: string;
-    item: JuegoItem;
-  }
-  
-  //Definición de los campos de interfaz por defecto de Sharepoint añadiendo SP y el WebPartContext
+import { DateTimePicker, DateConvention} from "@pnp/spfx-controls-react/lib/DateTimePicker";
+
+//Definición de las columnas y petición para extraer los datos de la misma.
+const columns = [
+  {
+    title: 'Acciones',
+    dataIndex: 'acciones',
+    key: 'Title',
+    render: () => {
+      return (<a href="//Aquí va la ruta al la página de sharepoint donde se mostraran los datos en profundidad de cada juego">Ver</a>)
+    }
+  },
+  {
+    title: 'Nombre',
+    dataIndex: 'Title',
+    key: 'Title',
+  },
+  {
+    title: 'Descripción',
+    dataIndex: 'J_Desc',
+    key: 'J_Desc',
+  },
+  {
+    title: 'Fecha lanzamiento',
+    dataIndex: 'J_Lanz',
+    key: 'J_Lanz',
+    render: (dateString: string) => moment(dateString).format('DD/MM/YYYY'),
+  },
+  {
+    title: 'Género',
+    dataIndex: 'J_Gen',
+    key: 'J_Gen',
+  },
+  {
+    title: 'Duración',
+    dataIndex: 'J_Dur',
+    key: 'J_Dur',
+  },
+];
+interface JuegoItem {
+  Title: string;
+  J_Desc: string;
+  J_Dur: string;
+  J_Gen: string;
+  J_Lanz: string;
+}
+export interface DataType {
+  key: string;
+  item: JuegoItem;
+}
+
+//Definición de los campos de interfaz por defecto de Sharepoint añadiendo SP y el WebPartContext
 export interface IWebpart1Props {
   description: string;
   isDarkTheme: boolean;
@@ -60,14 +69,18 @@ export interface IWebpart1Props {
   SP: SPFI;
   WebPartContext: WebPartContext;
 }
-  //Función para coger los datos de la lista ya creada de Sharepoint.
+//Función para coger los datos de la lista ya creada de Sharepoint.
 export default function HelloWorld(props: IWebpart1Props) {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState(null);
+  const [fecha, setFecha] = useState(null);
+  const [genero, setGenero] = useState<IDropdownOption>(null);
+  const [duracion, setDuracion] = useState(null);
+
 
   const [datos, setDatos] = React.useState<any>(null);
 
   React.useEffect(() => {
-
-
     cargarDatos();
   }, [])
 
@@ -100,9 +113,28 @@ export default function HelloWorld(props: IWebpart1Props) {
   const showModal = () => {
     setIsModalOpen(true);
   };
-
-  const handleOk = () => {
+  
+  const handleOk = async () => {
     setIsModalOpen(false);
+   
+    const fechaString = fecha.toISOString();
+    alert(nombre+descripcion+fechaString+genero.text+duracion.text);
+    console.log(fecha)
+    console.log(fechaString)
+    //Accion de guardar
+    var item: any = {};
+    item["Title"] = nombre;
+    item["J_Desc"] = descripcion;
+    item["J_Lanz"] = fechaString;
+    item["J_Gen"] = genero.text;
+    item["J_Dur"] = duracion.text;
+
+    try {
+      var lista: any = await props.SP.web.lists.getByTitle("Juegos")
+      await lista.items.add(item);
+    } catch (ex) {
+      console.log(ex);
+    }
   };
 
   const handleCancel = () => {
@@ -144,48 +176,61 @@ export default function HelloWorld(props: IWebpart1Props) {
     { key: '50a60', text: 'De 50 a 60 horas.' },
     { key: 'masde60', text: 'Más de 60 horas.' },
   ];
-
-  //Creación de un selector de fechas para nuestro pop-up
-  const [firstDayOfWeek] = React.useState(DayOfWeek.Sunday);
-
   const stackTokens: IStackTokens = { childrenGap: 20 };
 
-
-  //Return de nuestro código.
+  //Return del código.
   return (
     <div className={styles.webpart1}>
-      <Button type="primary" onClick={showModal}>
+      <Button className={styles.boton1} type="primary" onClick={showModal}>
         Añadir nuevo juego.
       </Button>
+      <a href="PÁGINA DE EVENTOS">
+      <Button className={styles.boton1} type="primary">
+        Ver proximos eventos.
+      </Button>
+      </a>
       <Modal title="Datos del juego a añadir" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <Stack horizontal tokens={stackTokens} styles={stackStyles}>
           <Stack {...columnProps}>
-            <TextField label="Nombre del juego." required />
-            <TextField label="Descripción." multiline rows={3} required />
+            <TextField value={nombre} onChange={(event, val) => setNombre(val)} label="Nombre del juego." required />
+            <TextField value={descripcion} onChange={(event, val) => setDescripcion(val)} label="Descripción." multiline rows={3} required />
             <Dropdown
-              required
               placeholder="Género."
               label="Género del juego."
+              selectedKey={genero?.key.toString()}
+              onChange={(event, val) => setGenero(val)}
               options={optionsgenero}
               styles={dropdownStyles}
             />
             <Dropdown
-              required
+              selectedKey={duracion?.key.toString()}
+              onChange={(event, val) => setDuracion(val)}
               placeholder="Duración."
               label="Duración del modo campaña."
               options={optionsduracion}
               styles={dropdownStyles}
             />
-            <DatePicker
-              firstDayOfWeek={firstDayOfWeek}
-              placeholder="Selecciona fecha de salida."
-              ariaLabel="Selecciona una fecha."
-              strings={defaultDatePickerStrings}
-            />
+            <DateTimePicker
+              showLabels={false}
+              label="Introduce fecha"
+              firstDayOfWeek={1}
+              value={fecha}
+              dateConvention={DateConvention.Date}
+              formatDate={(Date) => {
+              if (Date == null) return null;
+                return Date.toLocaleDateString();
+              }}
+              onChange={(Date) => {
+                setFecha(Date);
+              }}
+              allowTextInput={false}
+              showClearDate={true}
+            ></DateTimePicker>
           </Stack>
         </Stack>
       </Modal>
-      <Table dataSource={datos} columns={columns} />;
+      <Table dataSource={datos} columns={columns}>
+      </Table>
     </div>
   )
 }
